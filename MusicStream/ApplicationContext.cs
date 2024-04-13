cf334772057490aeb7f29a862e711989153543d6
MaratByd04 <143888006+MaratByd04@users.noreply.github.com>
2024-04-14 00:45:25 +0300

    Дизайн формы регистрации

---
+++ MusicStream/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MusicStream
+{
+    internal class ApplicationContext : DbContext
+    {
+        public DbSet <User> Users => Set<User>();
+        public ApplicationContext() => Database.EnsureCreated();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=users.db");
+        }  
+    }
+}
