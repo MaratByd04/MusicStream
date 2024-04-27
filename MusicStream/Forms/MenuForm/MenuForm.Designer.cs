fc33880d056aee3530a8869a7c5006d064442757
Roman <roman.rakov2909@gmail.com>
2024-04-27 22:21:07 +0300

    изменил фото

--- MusicStream/Forms/MenuForm/MenuForm.Designer.cs
+++ MusicStream/Forms/MenuForm/MenuForm.Designer.cs
@@ -36,9 +36,12 @@
             SadnessPlaylistButton = new Button();
             WorkPlaylistButton = new Button();
             SportPlaylistButton = new Button();
-            PlaylistListBox = new ListBox();
             HidePlaylistButton = new Button();
             SaveTrackButton = new Button();
+            button1 = new Button();
+            PlaylistListBox = new ListBox();
+            button2 = new Button();
+            button3 = new Button();
             SuspendLayout();
             // 
             // RecommendationButton
@@ -46,9 +49,10 @@
             RecommendationButton.FlatStyle = FlatStyle.Flat;
             RecommendationButton.Font = new Font("Modern No. 20", 13.7999992F, FontStyle.Bold);
             RecommendationButton.ForeColor = Color.PaleTurquoise;
-            RecommendationButton.Location = new Point(31, 98);
+            RecommendationButton.Location = new Point(27, 63);
+            RecommendationButton.Margin = new Padding(3, 2, 3, 2);
             RecommendationButton.Name = "RecommendationButton";
-            RecommendationButton.Size = new Size(260, 65);
+            RecommendationButton.Size = new Size(228, 60);
             RecommendationButton.TabIndex = 0;
             RecommendationButton.Text = "Посмотреть рекомендации";
             RecommendationButton.UseVisualStyleBackColor = true;
@@ -59,9 +63,10 @@
             TrackListButton.FlatStyle = FlatStyle.Flat;
             TrackListButton.Font = new Font("Modern No. 20", 13.7999992F, FontStyle.Bold);
             TrackListButton.ForeColor = Color.PaleTurquoise;
-            TrackListButton.Location = new Point(310, 96);
+            TrackListButton.Location = new Point(271, 63);
+            TrackListButton.Margin = new Padding(3, 2, 3, 2);
             TrackListButton.Name = "TrackListButton";
-            TrackListButton.Size = new Size(260, 65);
+            TrackListButton.Size = new Size(228, 58);
             TrackListButton.TabIndex = 1;
             TrackListButton.Text = "Избранное";
             TrackListButton.UseVisualStyleBackColor = true;
@@ -72,9 +77,10 @@
             CreateSongButton.FlatStyle = FlatStyle.Flat;
             CreateSongButton.Font = new Font("Modern No. 20", 13.7999992F, FontStyle.Bold);
             CreateSongButton.ForeColor = Color.PaleTurquoise;
-            CreateSongButton.Location = new Point(588, 96);
+            CreateSongButton.Location = new Point(514, 63);
+            CreateSongButton.Margin = new Padding(3, 2, 3, 2);
             CreateSongButton.Name = "CreateSongButton";
-            CreateSongButton.Size = new Size(260, 67);
+            CreateSongButton.Size = new Size(228, 59);
             CreateSongButton.TabIndex = 2;
             CreateSongButton.Text = "Добавить композицию в список сервиса";
             CreateSongButton.UseVisualStyleBackColor = true;
@@ -85,9 +91,10 @@
             ProfileButton.FlatStyle = FlatStyle.Flat;
             ProfileButton.Font = new Font("Modern No. 20", 13.7999992F, FontStyle.Bold);
             ProfileButton.ForeColor = Color.PaleTurquoise;
-            ProfileButton.Location = new Point(867, 96);
+            ProfileButton.Location = new Point(759, 63);
+            ProfileButton.Margin = new Padding(3, 2, 3, 2);
             ProfileButton.Name = "ProfileButton";
-            ProfileButton.Size = new Size(260, 65);
+            ProfileButton.Size = new Size(228, 58);
             ProfileButton.TabIndex = 3;
             ProfileButton.Text = "Профиль";
             ProfileButton.UseVisualStyleBackColor = true;
@@ -95,69 +102,57 @@
             // 
             // SadnessPlaylistButton
             // 
+            SadnessPlaylistButton.BackgroundImage = (Image)resources.GetObject("SadnessPlaylistButton.BackgroundImage");
+            SadnessPlaylistButton.BackgroundImageLayout = ImageLayout.Zoom;
             SadnessPlaylistButton.Font = new Font("Times New Roman", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            SadnessPlaylistButton.ForeColor = SystemColors.ActiveCaptionText;
-            SadnessPlaylistButton.Image = (Image)resources.GetObject("SadnessPlaylistButton.Image");
-            SadnessPlaylistButton.Location = new Point(128, 227);
-            SadnessPlaylistButton.Margin = new Padding(30, 10, 30, 10);
+            SadnessPlaylistButton.Location = new Point(112, 170);
+            SadnessPlaylistButton.Margin = new Padding(26, 8, 26, 8);
             SadnessPlaylistButton.Name = "SadnessPlaylistButton";
-            SadnessPlaylistButton.Size = new Size(259, 269);
+            SadnessPlaylistButton.Size = new Size(227, 202);
             SadnessPlaylistButton.TabIndex = 1;
-            SadnessPlaylistButton.Text = "Погрустить";
             SadnessPlaylistButton.TextAlign = ContentAlignment.BottomCenter;
             SadnessPlaylistButton.UseVisualStyleBackColor = true;
             SadnessPlaylistButton.Click += SadnessPlaylistButton_Click;
             // 
             // WorkPlaylistButton
             // 
+            WorkPlaylistButton.BackColor = Color.Transparent;
+            WorkPlaylistButton.BackgroundImage = (Image)resources.GetObject("WorkPlaylistButton.BackgroundImage");
+            WorkPlaylistButton.BackgroundImageLayout = ImageLayout.Zoom;
             WorkPlaylistButton.Font = new Font("Times New Roman", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 204);
             WorkPlaylistButton.ForeColor = SystemColors.ActiveCaptionText;
-            WorkPlaylistButton.Image = (Image)resources.GetObject("WorkPlaylistButton.Image");
-            WorkPlaylistButton.Location = new Point(447, 227);
-            WorkPlaylistButton.Margin = new Padding(30, 10, 30, 10);
+            WorkPlaylistButton.Location = new Point(391, 170);
+            WorkPlaylistButton.Margin = new Padding(26, 8, 26, 8);
             WorkPlaylistButton.Name = "WorkPlaylistButton";
-            WorkPlaylistButton.Size = new Size(274, 269);
+            WorkPlaylistButton.Size = new Size(240, 202);
             WorkPlaylistButton.TabIndex = 2;
-            WorkPlaylistButton.Text = "Для работы";
             WorkPlaylistButton.TextAlign = ContentAlignment.BottomCenter;
-            WorkPlaylistButton.UseVisualStyleBackColor = true;
+            WorkPlaylistButton.UseVisualStyleBackColor = false;
             WorkPlaylistButton.Click += WorkPlaylistButton_Click;
             // 
             // SportPlaylistButton
             // 
+            SportPlaylistButton.BackgroundImage = (Image)resources.GetObject("SportPlaylistButton.BackgroundImage");
+            SportPlaylistButton.BackgroundImageLayout = ImageLayout.Zoom;
             SportPlaylistButton.Font = new Font("Times New Roman", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 204);
             SportPlaylistButton.ForeColor = SystemColors.ActiveCaptionText;
-            SportPlaylistButton.Image = (Image)resources.GetObject("SportPlaylistButton.Image");
-            SportPlaylistButton.Location = new Point(781, 227);
-            SportPlaylistButton.Margin = new Padding(30, 10, 30, 10);
+            SportPlaylistButton.Location = new Point(683, 170);
+            SportPlaylistButton.Margin = new Padding(26, 8, 26, 8);
             SportPlaylistButton.Name = "SportPlaylistButton";
-            SportPlaylistButton.Size = new Size(277, 269);
+            SportPlaylistButton.Size = new Size(242, 202);
             SportPlaylistButton.TabIndex = 0;
-            SportPlaylistButton.Text = "Для спорта";
             SportPlaylistButton.TextAlign = ContentAlignment.BottomCenter;
             SportPlaylistButton.UseVisualStyleBackColor = true;
             SportPlaylistButton.Click += SportPlaylistButton_Click;
-            // 
-            // PlaylistListBox
-            // 
-            PlaylistListBox.BackColor = Color.Black;
-            PlaylistListBox.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            PlaylistListBox.ForeColor = Color.PaleTurquoise;
-            PlaylistListBox.FormattingEnabled = true;
-            PlaylistListBox.ItemHeight = 37;
-            PlaylistListBox.Location = new Point(31, 188);
-            PlaylistListBox.Name = "PlaylistListBox";
-            PlaylistListBox.Size = new Size(1093, 337);
-            PlaylistListBox.TabIndex = 4;
-            PlaylistListBox.Visible = false;
             // 
             // HidePlaylistButton
             // 
             HidePlaylistButton.BackColor = Color.Black;
             HidePlaylistButton.ForeColor = Color.PaleTurquoise;
-            HidePlaylistButton.Location = new Point(1023, 188);
+            HidePlaylistButton.Location = new Point(895, 141);
+            HidePlaylistButton.Margin = new Padding(3, 2, 3, 2);
             HidePlaylistButton.Name = "HidePlaylistButton";
-            HidePlaylistButton.Size = new Size(101, 29);
+            HidePlaylistButton.Size = new Size(88, 22);
             HidePlaylistButton.TabIndex = 5;
             HidePlaylistButton.Text = "Свернуть";
             HidePlaylistButton.UseVisualStyleBackColor = false;
@@ -168,22 +163,79 @@
             // 
             SaveTrackButton.BackColor = Color.Black;
             SaveTrackButton.ForeColor = Color.PaleTurquoise;
-            SaveTrackButton.Location = new Point(31, 548);
+            SaveTrackButton.Location = new Point(27, 411);
+            SaveTrackButton.Margin = new Padding(3, 2, 3, 2);
             SaveTrackButton.Name = "SaveTrackButton";
-            SaveTrackButton.Size = new Size(208, 60);
+            SaveTrackButton.Size = new Size(182, 45);
             SaveTrackButton.TabIndex = 6;
             SaveTrackButton.Text = "Добавить в избранное";
             SaveTrackButton.UseVisualStyleBackColor = false;
             SaveTrackButton.Visible = false;
             SaveTrackButton.Click += SaveTrackButton_Click;
             // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Modern No. 20", 11.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.PaleTurquoise;
+            button1.Location = new Point(153, 141);
+            button1.Margin = new Padding(3, 2, 3, 2);
+            button1.Name = "button1";
+            button1.Size = new Size(144, 32);
+            button1.TabIndex = 7;
+            button1.Text = "Погрустить";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // PlaylistListBox
+            // 
+            PlaylistListBox.BackColor = Color.Black;
+            PlaylistListBox.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            PlaylistListBox.ForeColor = Color.PaleTurquoise;
+            PlaylistListBox.FormattingEnabled = true;
+            PlaylistListBox.ItemHeight = 30;
+            PlaylistListBox.Location = new Point(27, 141);
+            PlaylistListBox.Margin = new Padding(3, 2, 3, 2);
+            PlaylistListBox.Name = "PlaylistListBox";
+            PlaylistListBox.Size = new Size(957, 244);
+            PlaylistListBox.TabIndex = 4;
+            PlaylistListBox.Visible = false;
+            // 
+            // button2
+            // 
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Modern No. 20", 11.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.PaleTurquoise;
+            button2.Location = new Point(440, 141);
+            button2.Margin = new Padding(3, 2, 3, 2);
+            button2.Name = "button2";
+            button2.Size = new Size(144, 32);
+            button2.TabIndex = 8;
+            button2.Text = "Для работы";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Modern No. 20", 11.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.ForeColor = Color.PaleTurquoise;
+            button3.Location = new Point(734, 141);
+            button3.Margin = new Padding(3, 2, 3, 2);
+            button3.Name = "button3";
+            button3.Size = new Size(144, 32);
+            button3.TabIndex = 9;
+            button3.Text = "Для спорта";
+            button3.UseVisualStyleBackColor = true;
+            // 
             // MenuForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Black;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
-            ClientSize = new Size(1168, 667);
+            ClientSize = new Size(1022, 500);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Controls.Add(SaveTrackButton);
             Controls.Add(HidePlaylistButton);
             Controls.Add(SadnessPlaylistButton);
@@ -195,6 +247,7 @@
             Controls.Add(RecommendationButton);
             Controls.Add(PlaylistListBox);
             FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(3, 2, 3, 2);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "MenuForm";
@@ -213,8 +266,11 @@
         private Button SportPlaylistButton;
         private Button SadnessPlaylistButton;
         private Button WorkPlaylistButton;
-        private ListBox PlaylistListBox;
         private Button HidePlaylistButton;
         private Button SaveTrackButton;
+        private Button button1;
+        private ListBox PlaylistListBox;
+        private Button button2;
+        private Button button3;
     }
 }
--- MusicStreamTest/obj/Debug/net8.0-windows8.0/MusicStreamTest.AssemblyInfo.cs
+++ MusicStreamTest/obj/Debug/net8.0-windows8.0/MusicStreamTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicStreamTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+505553aed18b0b9eceb541c649e453de3e621967")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0d1ffa0182f2265a958d27b50cb585206d5ce68")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicStreamTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicStreamTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
